9d0bae0769651ca04ae1c71fe979db9e794ed08b
TiaMutiara <93575026+TiaMutiara@users.noreply.github.com>
2022-03-21 06:58:40 +0700

    Add files via upload

---
+++ views/project.php
@@ -0,0 +1,29 @@
+<div class="container-fluid px-2 px-md-4">
+   <div class="card card-body min-height-400 border-radius-xl mt-4"> 
+   <div class="col-10">
+              <div class="card card-plain h-100">
+                <div class="card-header pb-0 p-3">
+                  <div class="row">
+                    <div class="col-md-8 d-flex align-items-center">
+                      <h6 class="mb-0">Project</h6>
+                    </div>
+                    <div class="col-md-4 text-end">
+                      <a href="javascript:;">
+                        <i class="bi bi-file-earmark-plus-fill"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+  <img src="../tia.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Tia Mutiara</h5>
+    <p class="card-text">aku hari ini sedang sekolah gaisssssss</p>
+    <a href="#" class="btn btn-primary">edit</a>
+    <a href="#" class="btn btn-primary">hapus</a>
+  </div>
+</div>
+              </div>
+            </div>
+   </div>
+</div>
