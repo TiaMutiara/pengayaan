e54e8c092e3b2e33282ae7475015fcb9ac247c8e
TiaMutiara <93575026+TiaMutiara@users.noreply.github.com>
2022-03-21 13:13:17 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,32 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "portofolio");
+$a = mysqli_query($koneksi, "SELECT * FROM user");
+$b = mysqli_query($koneksi, "SELECT * FROM about");
+
+
+function data($isi){
+    global $koneksi;
+    $c = mysqli_query($koneksi, $isi);
+    $box = [];
+    while ($keluarkan = mysqli_fetch_assoc($c)) 
+    {
+      $box[] = $keluarkan;
+    }
+    return $box;
+}
+
+function tambah($data){
+  global $koneksi;
+  $email = $data["email"];
+  $nama =  $data["nama_k"];
+  $pesan =  $data["pesan"];
+
+  $query = " INSERT INTO contact
+            VALUES 
+            ('', '$email', '$nama', '$pesan')
+            ";
+          mysqli_query ($koneksi, $query);
+          return mysqli_affected_rows($koneksi);
+
+}
+?>
