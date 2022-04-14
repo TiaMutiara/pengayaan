7b1ade6d3bae3f926e5a382924d6b220ab3cc61d
TiaMutiara <93575026+TiaMutiara@users.noreply.github.com>
2022-04-14 13:19:09 +0700

    Add files via upload

--- views/login.php
+++ views/login.php
@@ -7,8 +7,8 @@
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <title>
-    Material Dashboard 2 by Creative Tim
-  </title>
+  Login
+</title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
@@ -17,13 +17,14 @@
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <!-- Material Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">  <!-- CSS Files -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
   <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
 </head>
 
-<body class="bg-gray-200">
+<body class="bg-gray-200">                                                                          
 <?php
-  if(isset($_POST['login'])) {
+  if(isset($_POST['kirim'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
@@ -53,80 +54,42 @@
     <div class="row">
       <div class="col-12">
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-          <div class="container-fluid ps-2 pe-0">
-            <div class="collapse navbar-collapse" id="navigation">
-              <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../index.php">
-                  login moal ?
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        
         <!-- End Navbar -->
       </div>
     </div>
   </div>
   <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
+    <div class="page-header align-items-start min-vh-100"> <img :src="assets/img/walp.jpg">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container my-auto">
         <div class="row">
           <div class="col-lg-4 col-md-8 col-12 mx-auto">
             <div class="card z-index-0 fadeIn3 fadeInBottom">
-              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
-                  <div class="row mt-3">
-                    <div class="col-2 text-center ms-auto">
-                      <a class="btn btn-link px-3" href="https://id-id.facebook.com">
-                        <i class="fa fa-facebook text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center px-1">
-                      <a class="btn btn-link px-3" href="https://github.com">
-                        <i class="fa fa-github text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center me-auto">
-                      <a class="btn btn-link px-3" href="https://www.google.com">
-                        <i class="fa fa-google text-white text-lg"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <?=@$pesan_error?>
-                <form method="POST" class="text-start">
+              
+              <div class="card-body" id="app">
+              <?=@$pesan_error?>
+                <form method="POST" class="text-start"> 
                   <div class="input-group input-group-outline my-3">
                     <label class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control">
+                    <input v-model="email" type="email" name="email" class="form-control" >
                   </div>
-                  <div class="input-group input-group-outline mb-3">
+                  <div class="input-group input-group-outline mb-3" v-if="email.length > 8">
                     <label class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control">
+                    <input v-model="password" type="password" name="password" class="form-control" >
                   </div>
-                  <div class="form-check form-switch d-flex align-items-center mb-3">
-                    <input class="form-check-input" type="checkbox" id="rememberMe">
-                    <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember me</label>
+                  <em v-if="email">sedang mengecek</em>
+                  
+                  <div class="text-center" v-if="password.length > 3">
+                    <button type="submit" name="kirim" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
                   </div>
-                  <div class="text-center">
-                    <button type="submit" name="login" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
-                  </div>
-                  <p class="mt-4 text-sm text-center">
-                    Don't have an account?
-                    <a href="pages/sign-up.html" class="text-primary text-gradient font-weight-bold">Sign up</a>
-                  </p>
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   </main>
   <!--   Core JS Files   -->
@@ -134,6 +97,31 @@
   <script src="assets/js/core/bootstrap.min.js"></script>
   <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data() {
+        return {
+          email: '',
+          password: '',
+        }
+      },
+      watch: {
+        email() {
+          if(this.email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  )) {
+            console.log('email benar');
+            return true;
+          } else {
+            console.log('email salah');
+            return false;
+          }
+        }
+      }
+    })
+  </script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
